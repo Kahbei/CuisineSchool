8bf0dafa2a674cab9d64f8eb68f91df8ea189675
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2021-06-28 14:25:21 +0200

    Première version des models

--- models/cours.js
+++ models/cours.js
@@ -1,3 +1,13 @@
-/**
- * Model qui accueillera celui des cours
- */
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const Schema = mongoose.Schema;
+
+const coursSchema = new Schema({
+    idcours: {type: Integer},
+    recette: { type: String, min: 50, max: 2000 },
+    image: { type: String},
+});
+
+const cours = mongoose.model('cours', coursSchema);
+
+export { cours };
--- models/membre.js
+++ models/membre.js
@@ -1,3 +1,13 @@
-/**
- * Model qui accueillera celui des membres
- */
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    idmembre: {type: Integer},
+    username: { type: String, min: 4, max: 15 },
+    password: { type: String, required: true, max: 1024, min: 6}
+});
+
+const membre = mongoose.model('membre', userSchema);
+
+export { membre };
